7a5185737f7662df1a8cc4f64be667b53a69d734
Roman Mashta <roman.mashta@gmail.com>
2020-06-14 03:14:29 +0300

    Type guards implemented

--- src/meta-model.ts
+++ src/meta-model.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export type TypeInfo = ObjectType | SimpleType | ArrayType | TypeReference | EnumType | ConstLiteral | OneOf | AllOf;
 
 export interface TypeReference {
@@ -47,3 +49,14 @@ export interface MetaInfo {
   modules: Module[];
   hasErrors: boolean;
 }
+
+export const isObjectType = (typeInfo: TypeInfo): typeInfo is ObjectType => (typeInfo as ObjectType).type === 'object';
+export const isSimpleType = (typeInfo: TypeInfo): typeInfo is SimpleType =>
+  _.includes(['string', 'number', 'boolean', 'date', 'null'], (typeInfo as ObjectType).type);
+export const isArrayType = (typeInfo: TypeInfo): typeInfo is ArrayType => (typeInfo as ArrayType).type === 'array';
+export const isTypeReference = (typeInfo: TypeInfo): typeInfo is TypeReference =>
+  (typeInfo as TypeReference).$ref !== undefined;
+export const isEnumType = (typeInfo: TypeInfo): typeInfo is EnumType => (typeInfo as EnumType).enum !== undefined;
+export const isConstLiteral = (typeInfo: TypeInfo): typeInfo is ConstLiteral =>
+  (typeInfo as ConstLiteral).const !== undefined;
+export const isOneOf = (typeInfo: TypeInfo): typeInfo is OneOf => (typeInfo as OneOf).oneOf !== undefined;
